df0745bfb41abbaccca97b8790ac63ec2e0a381c
takagotch <dyaccb@gmail.com>
2019-05-31 10:32:26 +0900

    var Transaction = function() {} this.version = 1; this.lock_time = 0; this.ins = []; this.outs = []; this.timeouts = []; this.timestamp = null; this.block = null; if () this.hash = doc.hash;

--- fr/lib/btc/transaction.js
+++ fr/lib/btc/transaction.js
@@ -56,19 +56,207 @@ Transaction.prototype.addInput = function (tx, outIndex) {
 );
 
 Transaction.prototype.addOutput = funciton (address, value) {
-  if () {} else {}
+  if (arguments[0] instanceof TransactionOut) {
+    this.outs.push(arguments[0]);
+  } else {
+    if ("string" == typeof address) {
+      address = new Address(address);
+    }
+    if ("number" == typeof value) {
+      value = BigInteger.valueOf(value);
+    }
+
+    if (value instanceof BigInteger) {
+      value = value.toByteArrayUnsigned().reverse();
+      while (value.length < 8) value.push(0);
+    } else if (util.isArray(value)) {
+    }
+
+    this.outs.push(new TransactionOut({
+      value: value,
+      script: Script.createOutputScript(address)
+    }));
+  }
 };
 
-var bytesToWards = function () {};
 
 var wordsToBytes = function (words) {
+  for (var words = [], i = 0, b = 0; i < bytes.length; i++, b += 8)
+    words[b >> 5] |= bytes[i] << (24 - b % 32);
+  return words;
+};
 
+var wordsToBytes = function (words) {
+  for (var bytes = [], b = 0; b < words.length * 32; b += 8)
+    bytes.push((words[b >> 5] >>> (24 - b % 32)) & 0xFF);
+  return bytes;
+};
+
+Transaction.prototype.serialize = funciton ()
+{
+  var buffer = [];
+  buffer = buffer.concat(wordsBytes([parseInt(this.version)]).reverse());
+  buffer = buffer.concat(util.numToVarInt(this.ins.length));
+  for (var i = 0; i < this.ins.length; i++) {
+    var txin = this.ins[i];
+
+    buffer = buffer.concat(conv.hexToBytes(txin.outpoint.hash).reverse());
+    buffer = buffer.concat(wordsToBytes([parseInt(txin.outpoint.index)]).reverse());
+    var scriptBytes = txin.script.buffer;
+    buffer = buffer.concat(util.numToVarInt(scriptBytes.length));
+    buffer = buffer.concat(scriptBytes);
+    buffer = buffer.concat(wordsToBytes([parseInt(txin.sequence)]).reverse();)
+  }
+  buffer = buffer.concat(util.numToVarInt(this.outs.length));
+  for (var i = 0; i < this.outs.length; i++) {
+    var txout = this.outs[i];
+    buffer = buffer.concat(txout.value);
+    var scriptBytes = txout.script.buffer;
+    buffer = buffer.concat(util.numToVarInt(scriptBytes.length));
+    buffer = buffer.concat(scriptBytes);
+  }
+  buffer = buffer.concat(wordsToBytes([parseInt(this.lock_time)]).reverse());
+
+  return buffer;
+};
+
+var OP_CODESEPARATOR = 171;
+
+var SIGHASH_ALL = 1;
+var SIGHASH_NONE = 2;
+var SIGHASH_SINGLE = 3;
+var SIGHASH_ANYONECANPAY = 80;
+
+
+Transaction.prototype.hashTransactionForSignature = 
+function (connectedScript, inIndex, hashType) 
+{
+  var txTmp = this.clone();
+
+  for (var i = 0; i < txTmp.ins.length; i++) {
+    txTmp.ins[i].script = new Script();
+  }
+
+  txTmp.ins[inIndex].script = connectedScript;
+
+  if ((hashType & 0x1f) == SIGHASH_NONE) {
+    txTmp.outs = [];
+
+    for (var i = 0; i < txTmp.ins.length; i++)
+      if (i != inIndex)
+	txTmp.ins[i].sequence = 0;
+  } else if ((hashType & 0x1f) == SIGHASH_SINGLE) {
+  }
+
+  if (hashType & SIGHASH_ANYONECANPAY) {
+    txTmp.ins = [txTmp.ins[inIndex]];
+  }
+
+  var buffer = txTmp.serialize();
+
+  buffer = buffer.concat(wordsToByte([parseInt(hashType)]).reverse());
+
+  var hash1 = Crypto.SHA256(buffer, {asBytes: true});
+
+  return Crypto.SHA256(hash1, {asBytes: true});
+}
+
+
+Transaction.prototype.getHash = function () {
+  var buffer = this.serialize();
+  return Crypto.SHA256(Crypto.SHA256(buffer, {asBytes: true}), {asBytes: true}).reverse();
+}
+
+/*
+ *
+ * */
+
+Transaction.prototype.clone = function () {
+  var newTx = new Transaction();
+  newTx.version = this.version;
+  newTx.lock_time = this.lock_time;
+  for (var i = 0; i < this.ins.length; i++) {
+    var txin = this.ins[i].clone();
+    newTx.addInput(txin);
+  }
+  for (var i = 0; i < this.outs.length; i++) {
+    var txout = this.outs[i].clone();
+    newTx.addOutput(txout);
+  }
+  return newTx;
 };
 
 
+Transaction.prototype.analyze = function (wallet) {
+  var Wallet = require('./wallet');
+  if (!(wallet instanceof Wallet)) return null;
 
+  var allFromMe = true,
+  alltoMe = true,
+  firstRecvHash = null,
+  firstMeRecvHash = null,
+  firstSendHash = null;
 
+  for (var i = this.outs.length-1; i >= 0; i--) {
+    var txout = this.outs[i];
+    var hash = txout.script.simpleOutPubKeyHash();
+    if (!wallet.hasHash(hash)) {
+      allToMe = false;
+    } else {
+      firstMeRecvHash = hash;
+    }
+    firstRecvHash = hash;
+  }
+  for (var i = this.ins.length-1; i >= 0; i--) {
+    var txin = this.ins[i];
+    firstSendHash = txin.script.simpleToPubKeyHash();
+    if (!wallet.hasHash(firstSendHash)) {
+      allFromMe = false;
+      break;
+    }
+  }
 
+  var impact = this.calcImpact(wallet);
+
+  var analysis = {};
+
+  analysis.impact = impact;
+
+  if (impact.sign > 0 && impact.value.compareTo(BigInteger.ZERO) > 0) {
+    analysis.type = 'recv';
+    analysis.addr = new Bitcoin.Address(firstMeRecvHash);
+  } else if (allFromMe && allToMe) {
+    analysis.type = 'self';
+  } else if (allFrame) {
+    analysis.type = 'sent';
+    analysis.addr = new Bitcoin.Address(firstRecvHash);
+  } else {
+    analysis.type = "other";
+  }
+
+  return analysis;
+};
+
+Transaction.prototype.getDescription = functin (wallet) {
+  var analysis = this.analyze(wallet);
+
+  if (!analysis) return "";
+
+  switch (analysis.type) {
+	  case 'recv':
+		  return "+analysis.addr";
+		  break;
+	case 'sent':
+		  return "+analysis.addr";
+		  break;
+	case 'self':
+		  return "Payment to yourself";
+		  break;
+	case 'other':
+	  default:
+		  return "";
+  }
+};
 
 /*
  *
